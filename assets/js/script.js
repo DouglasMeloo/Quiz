fc45e29ecc648d675374b79a3cc1aac0d91d7587
Douglas Melo <douglasengmelo@gmail.com>
2023-06-26 18:43:01 +0000

    add event listeners to answer elementsand enable submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,8 +68,21 @@ const showQuestion = (question) => {
     });
 
     questionNumber.innerHTML = `
-    Question <span class="current">${
-        questions.indexOf(question) + 1
-    }</span><span class="total">/${questions.length}</span>
+    Question <span class="current">${questions.indexOf(question) + 1
+        }</span><span class="total">/${questions.length}</span>
     `;
+
+    const answersDiv = document.querySelectorAll(".answer");
+    answersDiv.forEach((answer) => {
+        answer.addEventListener("click", () => {
+            if (!answer.classList.contains("checked")) {
+                answersDiv.forEach((answer) => {
+                    answer.classList.remove("selected");
+                });
+                answer.classList.add("selected");
+                submitBtn.disabled = false;
+            }
+        });
+    });
+
 };
