e090965d931fb23e5f948726de4a9eada0f4c24b
Douglas Melo <douglasengmelo@gmail.com>
2023-06-26 10:20:00 +0000

    add implement progress function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 const progressBar = document.querySelector(".progress-bar"), 
       progressText = document.querySelector(".progress-bar")
 
-  
+const progress = (value) => {
+    const percentage = (value / time) * 100;
+    progressBar.style.width = `${percentage}%`;
+    progressText.innerHTML = `${value}`;
+};
